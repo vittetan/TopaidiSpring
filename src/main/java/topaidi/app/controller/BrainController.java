ab1d87dd7e7184e71fe91e1d783440c9e3cd8f5a
Amandine <amandine.humanbooster@gmail.com>
2019-01-11 15:56:14 +0100

    BrainController et brain welcome jsp

---
+++ src/main/java/topaidi/app/controller/BrainController.java
@@ -0,0 +1,28 @@
+package topaidi.app.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import topaidi.app.dao.BrainDao;
+
+@Controller
+@RequestMapping("/brain")
+public class BrainController {
+
+	
+	HttpSession session;
+	@Autowired
+	BrainDao bDao;
+	
+	
+	@RequestMapping("/welcome")
+	public String home(Model model) {
+			return "brain/welcome";
+ 	}
+	
+	
+}
--- src/main/webapp/WEB-INF/views/jsp/brain/welcome.jsp
+++ src/main/webapp/WEB-INF/views/jsp/brain/welcome.jsp
@@ -1,12 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 
+<jsp:include page="../include/header.jsp" />
+
+<div>
+
+<h1>WELCOME BRAIN</h1>			
+
+</div>
 </body>
 </html>
